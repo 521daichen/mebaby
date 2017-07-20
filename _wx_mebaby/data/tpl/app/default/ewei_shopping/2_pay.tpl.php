662944e081757ce7a1093e0dcb71dfd5a5bd5db7
Dv <4895001@qq.com>
2017-07-20 17:14:00 +0800

    add DS_Store

--- _wx_mebaby/data/tpl/app/default/ewei_shopping/2_pay.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php defined('IN_IA') or exit('Access Denied');?><style>
-body{padding-bottom:50px;}
-</style>
-<?php  $this->pay($params);?>
-<?php (!empty($this) && $this instanceof WeModuleSite) ? (include $this->template('footerbar', TEMPLATE_INCLUDEPATH)) : (include template('footerbar', TEMPLATE_INCLUDEPATH));?>
