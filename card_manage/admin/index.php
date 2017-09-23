7d4d1f62a3ab6345de984f133cab0681aa2b5ba4
Dv <4895001@qq.com>
2017-09-23 10:52:19 +0800

    修改nginx配置

--- card_manage/admin/index.php
+++ card_manage/admin/index.php
@@ -1,4 +1,4 @@
 <?php
 session_start();
 $_SESSION['right_enter'] = 1;
-header("Location: index.php?s=Admin/Login");
+header("Location: ../index.php?s=Admin/Login");
--- card_manage/index.php
+++ card_manage/index.php
@@ -3,7 +3,7 @@
  * 单入口系统
  */
 
-//error_reporting(0);
+error_reporting(0);
 @set_time_limit(240);
 //@ini_set("memory_limit",'-1');
 define('APP_DEBUG', true);				//是否开启调试模式
