31634286c112f70e78347a8efdb5b287cdb273fa
Dv <4895001@qq.com>
2017-09-29 14:12:56 +0800

    更新缓存获取方式

--- _wx_mebaby/framework/function/cache.func.php
+++ _wx_mebaby/framework/function/cache.func.php
@@ -33,9 +33,11 @@ function cache_type() {
 
 function cache_load($key, $unserialize = false) {
 	global $_W;
+	/*
 	if (!empty($_W['cache'][$key])) {
 		return $_W['cache'][$key];
 	}
+	*/
 	$data = $_W['cache'][$key] = cache_read($key);
 	if ($key == 'setting') {
 		$_W['setting'] = $data;
